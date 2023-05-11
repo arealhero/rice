730e2480cf80812faddecdb20384af97c8150683
Vladislav Sharshukov <vsharshukov@gmail.com>
2023-05-11 15:29:15 +0300

    neovim: background option moved to `colors.lua`

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -31,6 +31,8 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
   vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+  vim.opt.background = 'light';
 end
 
 ColorMyPencils()
--- neovim/.config/nvim/lua/vlad/set.lua
+++ neovim/.config/nvim/lua/vlad/set.lua
@@ -4,9 +4,6 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.opt.background = "dark";
--- vim.opt.background = "light";
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
