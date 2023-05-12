764501978f2d5cb1fea25340d1e1fed9f04c143e
Vladislav Sharshukov <vsharshukov@gmail.com>
2023-05-12 17:48:46 +0300

    neovim: telescope's bind changed

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -9,7 +9,7 @@ telescope.setup {
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader> ', builtin.git_files, {})
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
